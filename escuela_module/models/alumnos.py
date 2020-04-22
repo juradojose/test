afa0bcab6612054fb252fb5150da1fd8458c01b6
Jose Alberto Reyna <jose.reyna@g-tec.io>
2020-04-22 10:48:35 -0500

    Usando configuracion antes de regarla brutalmente

---
+++ escuela_module/models/alumnos.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+class EscuelaAlumno(models.Model):
+    _name="escuela_module.escuela_alumno"
+    _rec_name='nombre'
+    #_inherit=['mail.thread','mail.tracking.value']
+    _inherit=['res.partner']
+
+    imagen=fields.Binary('Fotografía')
+    nombre= fields.Char(string="Nombre" )
+    apellido_pat=fields.Char(string="Apellido Paterno")
+    apellido_mat=fields.Char(string="Apellido Materno")
+    fecha_ingreso=fields.Date(string="Fecha de Ingreso", default= lambda self: fields.datetime.now())
+    
+    activo= fields.Selection([
+        ('activo' , 'Activo'),
+        ('inactivo','Inactivo')
+    ],string="Activo")
+
+    promedio_acumulado= fields.Float(string="Promedio Acumulado", digits=(3,2))
+    direccion=fields.Char(string="Dirección" )
+    matricula=fields.Char(string="Matricula", required=True, copy=False, readonly=True,
+                   index=True, default=lambda self: _('New'))
+
+    grupo=fields.Many2one('escuela_module.escuela_grupos','Grupo de Alumno')
+    materias= fields.Many2many('escuela_module.materias',string="Materias Asignadas")
+
+    @api.model
+    def create(self, vals):
+        vals['matricula'] = self.env['ir.sequence'].next_by_code('generate_matricula') or _('New')
+        res = super(EscuelaAlumno, self).create(vals)
+        return res
+
+    
---
+++ escuela_module/models/maestros.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+
+class EscuelaMaestro(models.Model):
+    _name="escuela_module.maestro"
+    _rec_name="nombre"
+    
+    imagen=fields.Binary('Fotografía')
+    nombre= fields.Char(string="Nombre")
+    apellido_pat=fields.Char(string="Apellido Paterno")
+    apellido_mat=fields.Char(string="Apellido Materno")
+    fecha_ingreso=fields.Date(string="Fecha de Ingreso")
+    cedula=fields.Char(string="Cedula Profecional")
+    activo= fields.Selection([
+        ('activo' , 'Activo'),
+        ('inactivo','Inactivo')
+    ],string="Activo")
+    direccion=fields.Char(string="Dirección")
+    materias=fields.Many2many('escuela_module.materias',string='Materias Impartidas')
+    tutorados=fields.Many2many('escuela_module.escuela_alumno',string='Tutorados')
+    state = fields.Selection([
+            ('concept', 'Concept'),
+            ('started', 'Started'),
+            ('progress', 'In progress'),
+            ('finished', 'Done'),
+            ],default='concept', readonly=True , string='Status')
+    #This function is triggered when the user clicks on the button 'Set to concept'
+    @api.multi
+    def concept_progressbar(self):
+	self.ensure_one()
+	self.write({
+	    'state': 'concept',
+	})
+
+    #This function is triggered when the user clicks on the button 'Set to started'
+    @api.multi
+    def started_progressbar(self):
+	self.ensure_one()
+	self.write({
+	    'state': 'started'
+	})
+
+    #This function is triggered when the user clicks on the button 'In progress'
+    @api.multi
+    def progress_progressbar(self):
+	self.ensure_one()
+	self.write({
+	    'state': 'progress'
+	})
+
+    #This function is triggered when the user clicks on the button 'Done'
+    @api.multi
+    def done_progressbar(self):
+	self.ensure_one()
+	self.write({
+	    'state': 'finished',
+	})
+    
